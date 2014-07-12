da0cbd19a3b048a6861f09824546fbdd8b78fcb5
Brittany Miller <brittanydmiller@gmail.com>
2014-07-11 18:30:08 -0700

    Validates uniquesness of ticket code within the context of a single survey
    This will allow the user to choose the option of allowing voters to validate their votes by providing a ticket number. (To prevent duplicate votes or vote theft)

--- textroar/app/models/vote.rb
+++ textroar/app/models/vote.rb
@@ -2,4 +2,5 @@ class Vote < ActiveRecord::Base
   belongs_to :survey
   attr_accessible :phone, :choice, :latitude, :longitude, :ticket, :survey_id
   validates :phone, uniqueness: { scope: :survey_id }
+  validates :ticket, uniqueness: { scope: :survey_id }
 end
--- textroar/spec/vote_spec.rb
+++ textroar/spec/vote_spec.rb
@@ -39,7 +39,22 @@ describe Vote do
     expect(vote2).to have(1).errors_on(:phone)
   end
 
-  it 'is invalid with a duplicate ticket, in the scope of the survey'
+  it 'is invalid with a duplicate ticket, in the scope of the survey' do
+    survey = Survey.create(
+      title: 'Favorite Futurama Crew Member',
+      descr: 'Which core crew member is your favorite?',
+      vote_start: 'Thurs, 08 May 2014 00:29:23 +0000',
+      vote_end: 'Fri, 09 May 2014 00:29:23 +0000',
+      poll_key: 'SJ47%',
+      phone: '5556667777',
+      user_id: 2,
+      latitude: '37.784816',
+      longitude: '-122.397387') 
+    vote1 = survey.votes.create(phone: '3223223332', choice: 'The Professor', ticket: 'G444', survey_id: survey.id)
+    vote2 = survey.votes.build(phone: '4554554445', choice: 'Nibbler', ticket: 'G444', survey_id: survey.id)
+    expect(vote2).to have(1).errors_on(:ticket) 
+  end
+
   it 'is invaild with a latitude more than XX miles from survey location'
   it 'is invalid with a longitude more than XX miles from survey location'
 
