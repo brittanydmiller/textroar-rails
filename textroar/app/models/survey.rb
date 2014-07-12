21cbda5ce95a8360cef524007760ddbbfbfeb38a
Brittany Miller <brittanydmiller@gmail.com>
2014-07-11 17:27:24 -0700

    Adds lat_long method to combine two attributes into a coordinates array (with rspec test)

--- textroar/app/models/survey.rb
+++ textroar/app/models/survey.rb
@@ -2,4 +2,8 @@ class Survey < ActiveRecord::Base
   has_many :responses
   belongs_to :user
   attr_accessible :title, :descr, :vote_start, :vote_end, :poll_key, :phone, :user_id, :latitude, :longitude
+
+  def lat_long
+    [latitude, longitude]
+  end
 end
--- textroar/spec/survey_spec.rb
+++ textroar/spec/survey_spec.rb
@@ -8,4 +8,17 @@ describe Survey do
   it 'is invalid without a poll key'
   it 'is invalid with a duplicate poll key'
   it 'is invalid without a phone number'
+  it 'returns an array of lat/long coordinates' do
+    survey = Survey.new(
+      title: 'Favorite Futurama Crew Member',
+      descr: 'Which core crew member is your favorite?',
+      vote_start: 'Thurs, 08 May 2014 00:29:23 +0000',
+      vote_end: 'Fri, 09 May 2014 00:29:23 +0000',
+      poll_key: 'SJ45%',
+      phone: '5556667777',
+      user_id: 2,
+      latitude: '37.784816',
+      longitude: '-122.397387')
+    expect(survey.lat_long).to eq(['37.784816', '-122.397387'])
+  end
 end
