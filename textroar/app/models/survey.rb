8ad9e408937e88308401be944a2febf1aab43a6b
Brittany Miller <brittanydmiller@gmail.com>
2014-07-11 17:35:57 -0700

    Adds presence validations and tests to Survey model

--- textroar/app/models/survey.rb
+++ textroar/app/models/survey.rb
@@ -2,6 +2,7 @@ class Survey < ActiveRecord::Base
   has_many :responses
   belongs_to :user
   attr_accessible :title, :descr, :vote_start, :vote_end, :poll_key, :phone, :user_id, :latitude, :longitude
+  validates :title, :vote_start, :vote_end, :poll_key, :phone, :user_id, presence: true
 
   def lat_long
     [latitude, longitude]
--- textroar/spec/survey_spec.rb
+++ textroar/spec/survey_spec.rb
@@ -1,13 +1,46 @@
 require 'spec_helper'
 
 describe Survey do 
-  it 'is valid with a title, description, start, end, poll_key, and phone'
-  it 'is invalid without a title'
-  it 'is invalid without a start'
-  it 'is invalid without an end'
-  it 'is invalid without a poll key'
+  it 'is valid with a title, description, start, end, poll_key, and phone' do
+    survey = Survey.new(
+      title: 'Favorite Futurama Crew Member',
+      descr: 'Which core crew member is your favorite?',
+      vote_start: 'Thurs, 08 May 2014 00:29:23 +0000',
+      vote_end: 'Fri, 09 May 2014 00:29:23 +0000',
+      poll_key: 'SJ45%',
+      phone: '5556667777',
+      user_id: 2,
+      latitude: '37.784816',
+      longitude: '-122.397387')
+    expect(survey).to be_valid
+  end
+
+  it 'is invalid without a title' do
+    expect(Survey.new(title: nil)).to have(1).errors_on(:title)
+  end
+
+  it 'is invalid without a start' do
+    expect(Survey.new(vote_start: nil)).to have(1).errors_on(:vote_start)
+  end
+
+  it 'is invalid without an end' do
+    expect(Survey.new(vote_end: nil)).to have(1).errors_on(:vote_end)
+  end
+  
+  it 'is invalid without a poll key' do
+    expect(Survey.new(poll_key: nil)).to have(1).errors_on(:poll_key)
+  end
+  
   it 'is invalid with a duplicate poll key'
-  it 'is invalid without a phone number'
+  
+  it 'is invalid without a phone number' do
+    expect(Survey.new(phone: nil)).to have(1).errors_on(:phone)
+  end
+
+  it 'is invalid wihtout a user_id' do
+    expect(Survey.new(user_id:nil)).to have(1).errors_on(:phone)
+  end
+
   it 'returns an array of lat/long coordinates' do
     survey = Survey.new(
       title: 'Favorite Futurama Crew Member',
