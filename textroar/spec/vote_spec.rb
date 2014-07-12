1f72e42f250addc895214acd280156908e368cb2
Brittany Miller <brittanydmiller@gmail.com>
2014-07-11 18:20:11 -0700

    Adds a differentiated test for a vote with full attributes vs just the minimum attrs

--- textroar/spec/vote_spec.rb
+++ textroar/spec/vote_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Vote do
-  it 'is valid with a phone and choice' do 
+  it 'is valid with a phone and choice (and all optional attributes)' do 
     vote = Vote.new(
       phone: '3443443444',
       choice: 'Bart',
@@ -11,7 +11,13 @@ describe Vote do
       survey_id: 44)
     expect(vote).to be_valid
   end
-  it 'is invalid with a duplicate phone, in the scope of the survey'
+
+  it 'is valid with only a phone and a choice' do
+    vote = Vote.new(phone: '1221221112', choice: 'Lisa')
+    expect(vote).to be_valid
+  end
+
+  it 'is invalid with a duplicate phone, in the scope of the survey' 
   it 'is invalid with a duplicate ticket, in the scope of the survey'
   it 'is invaild with a latitude more than XX miles from survey location'
   it 'is invalid with a longitude more than XX miles from survey location'
