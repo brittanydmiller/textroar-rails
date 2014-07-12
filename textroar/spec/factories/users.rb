0e6f639b3adf74783e04b26fbeb3edaea182a1db
Brittany Miller <brittanydmiller@gmail.com>
2014-07-11 22:21:32 -0700

    Adds Faker and integrates factory usage into User model specs

--- textroar/spec/factories/users.rb
+++ textroar/spec/factories/users.rb
@@ -1,8 +1,10 @@
+require 'faker'
+
 FactoryGirl.define  do
   factory :user do
     sequence(:username) {|n| "elena#{n}"}
     password_hash "gopro"
     sequence(:phone) {|n| "55#{n}5555555".truncate(10)} 
-    sequence(:email) {|n| "elena#{n}@example.com"}
+    email { Faker::Internet.email }
   end
 end
--- textroar/spec/user_spec.rb
+++ textroar/spec/user_spec.rb
@@ -11,26 +11,26 @@ describe User do
   end
 
   it 'has a valid factory' do
-    expect(FactoryGirl.build(:user)).to be_valid
+    expect(build(:user)).to be_valid
   end
 
   it 'is invalid without a username' do 
-    user = FactoryGirl.build(:user, username: nil)
+    user = build(:user, username: nil)
     expect(User.new(username: nil)).to have(1).errors_on(:username)
   end
 
   it 'is invalid without a password' do 
-    user = FactoryGirl.build(:user, password_hash: nil)
+    user = build(:user, password_hash: nil)
     expect(user).to have(1).errors_on(:password_hash)
   end
 
   it 'is invalid without a phone number' do
-    user = FactoryGirl.build(:user, phone: nil) 
+    user = build(:user, phone: nil) 
     expect(user).to have(1).errors_on(:phone)
   end
 
   it 'is invalid without an email address' do 
-    user = FactoryGirl.build(:user, email: nil)
+    user = build(:user, email: nil)
     expect(user).to have(1).errors_on(:email)
   end
 
@@ -44,17 +44,17 @@ describe User do
     end
 
     it 'is invalid with a duplicate phone number' do
-      user = FactoryGirl.build(:user, phone:'9339339993')
+      user = build(:user, phone:'9339339993')
       expect(user).to have(1).errors_on(:phone)
     end
 
     it 'is invalid with a duplicate email address' do
-      user = FactoryGirl.build(:user, email:'example@test.com')
+      user = build(:user, email:'example@test.com')
       expect(user).to have(1).errors_on(:email)
     end
 
     it 'is invalid with a duplicate username' do
-    user = FactoryGirl.build(:user, username:'Mauritzio')
+    user = build(:user, username:'Mauritzio')
     expect(user).to have(1).errors_on(:username)
   end
   end
