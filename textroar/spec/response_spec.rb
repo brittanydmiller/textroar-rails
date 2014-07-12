d8e56bddf631609a21a429bce1e67d2b80341262
Brittany Miller <brittanydmiller@gmail.com>
2014-07-11 19:43:32 -0700

    DRYs up duplication tests on Response model

--- textroar/spec/response_spec.rb
+++ textroar/spec/response_spec.rb
@@ -21,53 +21,40 @@ describe Response do
     expect(Response.new(response_key: nil)).to have(1).errors_on(:response_key)
   end
 
-  it 'is invalid with a duplicate key, in the scope of the survey' do
-    survey = Survey.create(
-      title: 'Favorite Futurama Crew Member',
-      descr: 'Which core crew member is your favorite?',
-      vote_start: 'Thurs, 08 May 2014 00:29:23 +0000',
-      vote_end: 'Fri, 09 May 2014 00:29:23 +0000',
-      poll_key: 'SJ45%',
-      phone: '5556667777',
-      user_id: 2,
-      latitude: '37.784816',
-      longitude: '-122.397387'
-      )
-    survey.responses.create(
-      survey_id: survey.id,
-      response_text: 'Bender',
-      response_key: '92')
+  describe "prevents duplicate responses in the same survey" do
 
-    response = survey.responses.build(
-      survey_id: survey.id,
-      response_text: 'Leela',
-      response_key: '92')
-      
-    expect(response).to have(1).errors_on(:response_key)
-  end
+    before :each do
+      @survey = Survey.create(
+        title: 'Favorite Futurama Crew Member',
+        descr: 'Which core crew member is your favorite?',
+        vote_start: 'Thurs, 08 May 2014 00:29:23 +0000',
+        vote_end: 'Fri, 09 May 2014 00:29:23 +0000',
+        poll_key: 'SJ45%',
+        phone: '5556667777',
+        user_id: 2,
+        latitude: '37.784816',
+        longitude: '-122.397387'
+        )
+      @survey.responses.create(
+        survey_id: @survey.id,
+        response_text: 'Bender',
+        response_key: '92')
+    end
 
-  it 'is invalid with duplicate text, in the scope of the survey' do  
-    survey = Survey.create(
-      title: 'Favorite Simpson',
-      descr: 'Which core family member is your favorite?',
-      vote_start: 'Thurs, 08 May 2014 00:29:23 +0000',
-      vote_end: 'Fri, 09 May 2014 00:29:23 +0000',
-      poll_key: 'SJ45%',
-      phone: '5556667777',
-      user_id: 2,
-      latitude: '37.784816',
-      longitude: '-122.397387'
-      )
-    survey.responses.create(
-      survey_id: survey.id,
-      response_text: 'Marge',
-      response_key: '448')
+    it 'is invalid with a duplicate key, in the scope of the survey' do
+      response = @survey.responses.build(
+        survey_id: @survey.id,
+        response_text: 'Leela',
+        response_key: '92')        
+      expect(response).to have(1).errors_on(:response_key)
+    end
 
-    response = survey.responses.build(
-      survey_id: survey.id,
-      response_text: 'Marge',
-      response_key: '447')
-      
-    expect(response).to have(1).errors_on(:response_text)
+    it 'is invalid with duplicate text, in the scope of the survey' do  
+      response = @survey.responses.build(
+        survey_id: @survey.id,
+        response_text: 'Bender',
+        response_key: '447')  
+      expect(response).to have(1).errors_on(:response_text)
+    end
   end
 end
