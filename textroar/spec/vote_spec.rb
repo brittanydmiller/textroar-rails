50c878900c2e4efa3433d94c5abfc4b4dc004002
Brittany Miller <brittanydmiller@gmail.com>
2014-07-11 17:18:31 -0700

    Adds pending rspec tests for Vote model

---
+++ textroar/spec/vote_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Vote do
+  it 'is valid with a phone and choice'
+  it 'is invalid with a duplicate phone, in the scope of the survey'
+  it 'is invalid with a duplicate ticket, in the scope of the survey'
+  it 'is invaild with a latitude more than XX miles from survey location'
+  it 'is invalid with a longitude more than XX miles from survey location'
+
+
+end
