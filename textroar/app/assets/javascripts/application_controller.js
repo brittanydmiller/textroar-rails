83ba2e3516f8d548f4f1a71f7fb5e3c96e820d6f
Brittany Miller <brittanydmiller@gmail.com>
2014-08-13 11:06:43 -0700

    Breaks out FormController and FormView
    * Overhauls ApplicationController to slim it down
    * Adds FormController and FormView
    * Cleans up Initialize to instantiate ApplicationController, FormController, and FormView
    * Pulls out jQuery selectors so they can be easily changed if needed

--- textroar/app/assets/javascripts/application_controller.js
+++ textroar/app/assets/javascripts/application_controller.js
@@ -1,33 +1,9 @@
-function ApplicationController() {
+function ApplicationController(formController) {
+  this.formController = formController
 }
 
 ApplicationController.prototype = {
   initialize: function() {
-    $('#open').on('click', this.openVoting);
-    $('#close').on('click', this.closeVoting);
-  },
-
-  openVoting: function(){
-    var startDate = new Date();
-  },
-  
-  closeVoting: function(e){
-    e.preventDefault();
-    var endDate = new Date();
-    // var authRequest = $.ajax({
-    //  url: '/get_auth',
-    //  type: 'GET'
-    // });
-    // authRequest.done(this.parseMessages);
-
-
-    // var ajaxRequest = $.ajax({
-    //  url: 'https://api.twilio.com/2010-04-01/Accounts/' + session["id"] + '/Messages/',
-    //  type: 'GET'
-    // });
-    // ajaxRequest.done(this.parseMessages);
-  }, 
-  parseMessages: function(e){
-    console.log(e);
+    this.formController.initialize()
   }
 }
--- textroar/app/assets/javascripts/initialize.js
+++ textroar/app/assets/javascripts/initialize.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
-  ApplicationController = new ApplicationController();
+  ApplicationController = new ApplicationController(new FormController(new FormView(formViewSelectors)));
   ApplicationController.initialize();
 })
 
 var formViewSelectors = {
-  // addForm: '#add-form',
-  // cancelForm: '#cancel-form',
-  // transactionForm: '#transaction-form'
+  addForm: '#add-form',
+  cancelForm: '#cancel-form',
+  form: '#response-form'
 }
+
+
