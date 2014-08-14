b7f09adcf55717c8002b5bc4668121350cb28900
Brittany Miller <brittanydmiller@gmail.com>
2014-08-13 17:07:20 -0700

    Nests response route inside survey route and fixes add_response form to match.

--- textroar/app/controllers/responses_controller.rb
+++ textroar/app/controllers/responses_controller.rb
@@ -1,6 +1,11 @@
 class ResponsesController < ApplicationController
   def create
-    p "inside response#create"
-    p params
+    @response = Response.new(text: params["text"], key: params["key"])
+    @response[:survey_id] = params["survey_id"]
+    if @response.save
+      #redirect_to @survey, :notice => "Successfully added response."
+    else
+      #render action: 'new'
+    end
   end
 end
