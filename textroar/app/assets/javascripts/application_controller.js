9e1e4930e90ce46851c713530cfb590f8ae54022
Brittany Miller <brittanydmiller@gmail.com>
2014-08-13 17:46:11 -0700

    Achieves a visual representation of the new response, although it is not yet accurate

--- textroar/app/assets/javascripts/application_controller.js
+++ textroar/app/assets/javascripts/application_controller.js
@@ -7,5 +7,9 @@ ApplicationController.prototype = {
   initialize: function() {
     this.votingController.initialize()
     this.formController.initialize()
+    $('#add-response').on('ajax:success', this.updateSurvey.bind(this))
+  },
+  updateSurvey: function(e, response){
+    $("#survey-responses").append("<li>" + response.key + " " + response.text + "</li>")
   }
 }
